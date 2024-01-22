84c6e98391782f0b768d1a71da28aaab1e7a3cc3
@jerscape <jeremydutton5@gmail.com>
2024-01-22 18:16:09 -0500

    exercise 1 completed, confirmed 3 stores in stores table using count method

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -10,6 +10,9 @@ burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: tr
 richmond = Store.create(name: "Richmond", annual_revenue: 126000 , mens_apparel: false , womens_apparel: true )
 gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true , womens_apparel: false)
 
+puts "this is the count of stores: #{Store.count}"
+
+#store stable structure for reference
 # t.column :name, :string
 # t.column :annual_revenue, :integer
 # t.column :mens_apparel, :boolean
