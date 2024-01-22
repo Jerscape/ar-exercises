5376800a2a27745e6b7c1415000e0f1f8d968d8e
@jerscape <jeremydutton5@gmail.com>
2024-01-22 18:54:44 -0500

    exercise 3 complete

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,15 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+
+puts "this is the count of stores: #{Store.count}"
+@store3 = Store.find_by(id: 3)
+@store3.destroy
+puts "this is the NEW count of stores: #{Store.count}"
+
+#store stable structure for reference
+# t.column :name, :string
+# t.column :annual_revenue, :integer
+# t.column :mens_apparel, :boolean
+# t.column :womens_apparel, :boolean
+# t.timestamps null: false
