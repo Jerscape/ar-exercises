44b9b5c343a095ee73fb8936b5cb518256bb79ef
@jerscape <jeremydutton5@gmail.com>
2024-01-22 22:56:02 -0500

    finished exercise 7, this one took a while

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,4 +1,32 @@
 class Employee < ActiveRecord::Base
 
-  belongs_to :store
+  validate :always_first_name
+  validate :always_last_name
+  validate :range_hourly_rate
+  validate :belongs_to_store
+
+  def always_first_name
+    if first_name.blank?
+      errors.add(:first_name, "must have a value")
+    end
+  end
+  
+  def always_last_name
+    if last_name.blank?
+      errors.add(:last_name, "must have a value")
+    end
+  end
+  
+  def range_hourly_rate
+    if hourly_rate < 40 || hourly_rate > 200
+      errors.add(:hourly_rate, "must be between 40 and 200")
+    end
+  end 
+
+  def belongs_to_store
+    if store_id.nil?
+      errors.add(:store, "must be belog to a store")
+    end
+  end 
+
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,4 +1,26 @@
 class Store < ActiveRecord::Base
   has_many :employees
-  
+
+  validate :store_name_length
+  validate :revenue_validation
+
+
+  def store_name_length
+    if name.length < 3
+      errors.add(:name, "Store numbers must be a minimum length of 3 characters")
+    end 
+  end
+
+  def revenue_validation
+    #chatGPT assisted with this conditional
+    if annual_revenue.nil? ||annual_revenue < 0 || !annual_revenue.is_a?(Integer)
+      errors.add(:name, "annual revenue must be greater than zero")
+
+      errors.full_messages.each do |message|
+        puts message
+      end
+      
+    end 
+
+  end
 end
